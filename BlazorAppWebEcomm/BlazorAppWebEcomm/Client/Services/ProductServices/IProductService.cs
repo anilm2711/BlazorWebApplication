43b0fe3731badfcd98604c152478e02ccbc7aeef
Anil Maurya <anil2711@hotmail.com>
2022-03-01 16:07:30 +0530

    adding search on client

--- BlazorAppWebEcomm/BlazorAppWebEcomm/Client/Services/ProductServices/IProductService.cs
+++ BlazorAppWebEcomm/BlazorAppWebEcomm/Client/Services/ProductServices/IProductService.cs
@@ -4,9 +4,15 @@
     {
         event Action ProductsChanged;
         List<Product> Products { get; set; }
+
+        string Message { get; set; }
         Task GetProducts();
         Task<ServiceResponse<Product>> GetProduct(int productId);
 
+        Task SearchProducts(string  searchText);
+
         Task GetProductByCategory(string categoryUrl);
+
+        Task<List<string>> GetProductSearchSuggestion(string searchText);
     }
 }
--- BlazorAppWebEcomm/BlazorAppWebEcomm/Client/Services/ProductServices/ProductService.cs
+++ BlazorAppWebEcomm/BlazorAppWebEcomm/Client/Services/ProductServices/ProductService.cs
@@ -13,6 +13,7 @@ namespace BlazorAppWebEcomm.Client.Services.ProductServices
             this._httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
         }
         public List<Product> Products { get; set; }
+        public string Message { get; set; }
 
         public async Task GetProducts()
         {
@@ -33,8 +34,25 @@ namespace BlazorAppWebEcomm.Client.Services.ProductServices
             var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/Product/GetProductsByCategory/{categoryUrl}");
             if (result != null && result.Data != null)
                 Products = result.Data;
-            ProductsChanged.Invoke();
+            ProductsChanged?.Invoke();
         }
 
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/Product/SearchProducts/{searchText}");
+            if (result != null && result.Data != null)
+                Products = result.Data;
+            if (Products.Count == 0)
+            {
+                Message = "No Products found";
+            }
+            ProductsChanged?.Invoke();
+        }
+
+        public async Task<List<string>> GetProductSearchSuggestion(string searchText)
+        {
+            var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/Product/GetProductSearchSuggestion/{searchText}");
+            return result.Data;
+        }
     }
 }
