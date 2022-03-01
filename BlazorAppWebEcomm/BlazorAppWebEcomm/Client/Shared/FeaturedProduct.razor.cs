7a9a13fb95ca0660dcb67c75ad2396b349cd634e
Anil Maurya <anil2711@hotmail.com>
2022-03-01 23:27:10 +0530

    featured product

---
+++ BlazorAppWebEcomm/BlazorAppWebEcomm/Client/Shared/FeaturedProduct.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorAppWebEcomm.Client.Shared
+{
+    public partial class FeaturedProduct : ComponentBase,IDisposable
+    {
+        [Inject]
+        public IProductService _productService { get; set; }
+
+        protected override void OnInitialized()
+        {
+            _productService.ProductsChanged += StateHasChanged;
+        }
+        public void Dispose()
+        {
+            _productService.ProductsChanged -= StateHasChanged;
+        }
+    }
+}
