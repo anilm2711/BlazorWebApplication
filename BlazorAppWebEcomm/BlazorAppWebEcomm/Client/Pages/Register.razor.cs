694451dc25da824ea419dd6c403459c7f1ccdd63
Anil Maurya <anil2711@hotmail.com>
2022-03-06 12:29:40 +0530

    Adding register menu besides search btn

---
+++ BlazorAppWebEcomm/BlazorAppWebEcomm/Client/Pages/Register.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorAppWebEcomm.Client.Pages
+{
+    public partial class Register : ComponentBase
+    {
+        public UserRegister user { get; set; } = new UserRegister();
+
+        void HandleRegistration()
+        {
+            Console.WriteLine("register");
+        }
+    }
+}
---
+++ BlazorAppWebEcomm/BlazorAppWebEcomm/Client/Shared/UserButton.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorAppWebEcomm.Client.Shared
+{
+    public partial class UserButton : ComponentBase
+    {
+        private bool showUserMenu = false;
+
+        private string UserMenuCssClass => showUserMenu ? "show-menu" : null;
+        private void ToggleUserMenu()
+        {
+            showUserMenu = !showUserMenu;
+        }
+
+        private async Task HideUserMenu()
+        {
+            await Task.Delay(200);
+            showUserMenu = false;
+        }
+
+    }
+}
